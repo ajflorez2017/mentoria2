d5da2545fecba9e9c75ad68c72c9e68ebb457146
ajflorez2017 <ajflorez2017@gmail.com>
2023-07-17 11:45:29 -0500

    issue with sg

--- main.tf
+++ main.tf
@@ -217,6 +217,7 @@ resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.ALB.arn
   port = 80
   protocol = "HTTP"
+  
 
   # By default, return a simple 404 page
   default_action {
@@ -235,6 +236,7 @@ resource "aws_lb_listener" "http" {
 resource "aws_security_group" "lb" {
   name        = "${var.environment}-default-lb"
   description = "Default SG to load balancer"
+  vpc_id =  aws_vpc.vpc_mentoring2.id
   
 
   ingress {
@@ -247,7 +249,7 @@ resource "aws_security_group" "lb" {
   egress {
     from_port = "80"
     to_port   = "80"
-    protocol  = "-1"
+    protocol  = "ALL"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
